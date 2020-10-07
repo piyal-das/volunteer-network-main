6fe6bdaaf510587180d5eb9c1db0bd5b4368bdd1
piyal-das <66130612+piyal-das@users.noreply.github.com>
2020-10-07 17:14:45 +0600

    somehow work

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,11 @@
-import React, { useContext } from 'react';
-import { Grid } from '@material-ui/core';
+import React, { useContext, useState } from 'react';
+import { Button, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Album from '../Album/Album';
 import SearchBox from '../SearchBox/SearchBox';
 import Navbar from '../Navbar/Navbar';
 import { userContext } from '../../App';
+import fakeDAta from '../../fakeData/gallery'
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -20,16 +21,26 @@ const useStyles = makeStyles((theme) => ({
 
 
 const Home = () => {
+    
     const classes = useStyles()
     const {allEvents} = useContext(userContext)
     const [events, setEvents] = allEvents
     // console.log(events)
 
     const colorSheet = ["#d35400","#8e44ad", "#9b59b6","#2ecc71", "#8c7ae6","#273c75","#d35400","#8e44ad", "#9b59b6","#2ecc71", "#8c7ae6","#273c75","#d35400","#8e44ad", "#9b59b6","#2ecc71", "#8c7ae6","#273c75","#8c7ae6","#d35400","#273c75","#8c7ae6","#273c75","#8c7ae6","#273c75"]
-    
+   
+    const Add = () => {
+        fetch('http://localhost:5000/Add', {
+            method: "POST",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(fakeDAta)
+        })
+        console.log(fakeDAta)
+    }
     return (
         <div>
             <Navbar></Navbar>
+            <Button className="btn btn-primary" onClick={Add}>Add</Button>
             <SearchBox></SearchBox>
             <div className={classes.root}>
                 <Grid container spacing={3}>
